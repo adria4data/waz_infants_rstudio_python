ff6e27f67eb58b62756bf7aa15fcfa4f9573ad3e
adria4data <141827371+adria4data@users.noreply.github.com>
2023-09-21 06:02:29 -0500

    Setting up the RStudio environment for Python

---
+++ script/waz_script_p.py
@@ -0,0 +1,16 @@
+# Predictors of Weight in RomanianInfants: Weight-for-age z-scores (WAZ)
+# Adrian Rus
+
+# September 2023
+
+
+# 1. Import major packages----------------------------------------------------------
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# 2. 
+
+pwd
